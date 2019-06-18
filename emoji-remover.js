fe9dbddd138f2cd02f232f4eb2526b1b32672fa0
Fabian Schmidberger <schmidberger.fabian@gmail.com>
2019-06-18 23:22:55 +0200

    :fire: remove function

--- emoji-remover.js
+++ emoji-remover.js
@@ -3,19 +3,11 @@
 var ranges = [
     '\ud83d[\ude00-\udeff]',
     '\ud83d[\udc00-\udcff]',
-    '\ud83c[\udf00-\udfff]'
+    '\ud83c[\udf00-\udff]'
 ];
 
 function removeEmojis(value){
     value = value.replace(new RegExp(ranges.join('|'), 'g'), '')
 
     return value;
-}
-
-function findSurrogatePair(point) {
-    // assumes point > 0xffff
-    var offset = point - 0x10000,
-        lead = 0xd800 + (offset >> 10),
-        trail = 0xdc00 + (offset & 0x3ff);
-    return [lead.toString(16), trail.toString(16)];
 }
