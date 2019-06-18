dbeb21e6127fd98b0d88d91fe647e0c0dc0517fa
Fabian Schmidberger <schmidberger.fabian@gmail.com>
2019-06-18 22:09:36 +0200

    :sparkles: js minify

--- emoji-remover.js
+++ emoji-remover.js
@@ -1,9 +1,7 @@
 // Version 0.0.1-alpha
 
 var ranges = [
-    '\ud83c[\udf00-\udfff]', // U+1F300 to U+1F3FF
-    '\ud83d[\udc00-\ude4f]', // U+1F400 to U+1F64F
-    '\ud83d[\ude80-\udeff]'  // U+1F680 to U+1F6FF
+    '\ud83c[\udf00-\udfff]' // U+1F300 to U+1F3FF
 ];
 
 function removeEmojis(value){
