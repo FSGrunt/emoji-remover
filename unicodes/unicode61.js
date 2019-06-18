be96516aeeae6461c06b47b5599b45796b42c340
Fabian Schmidberger <schmidberger.fabian@gmail.com>
2019-06-18 23:16:42 +0200

    :construction: added unicode 6.1

---
+++ unicodes/unicode61.js
@@ -0,0 +1,30 @@
+function getUnicode61(){
+    var unicode = "😀\n" +
+        "Grinning Face \n" +
+        "😗\n" +
+        "Kissing Face \n" +
+        "😙\n" +
+        "Kissing Face With Smiling Eyes \n" +
+        "😛\n" +
+        "Face With Tongue \n" +
+        "😑\n" +
+        "Expressionless Face \n" +
+        "😬\n" +
+        "Grimacing Face \n" +
+        "😴\n" +
+        "Sleeping Face \n" +
+        "😕\n" +
+        "Confused Face \n" +
+        "😟\n" +
+        "Worried Face \n" +
+        "😮\n" +
+        "Face With Open Mouth \n" +
+        "😯\n" +
+        "Hushed Face \n" +
+        "😦\n" +
+        "Frowning Face With Open Mouth \n" +
+        "😧\n" +
+        "Anguished Face";
+
+    return unicode;
+};
