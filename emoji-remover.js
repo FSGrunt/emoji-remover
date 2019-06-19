6401ce07b72480ee775cd572798306fd9cb04082
Fabian Schmidberger <schmidberger.fabian@gmail.com>
2019-06-19 08:33:26 +0200

    new unicodes for 6.0 added

--- emoji-remover.js
+++ emoji-remover.js
@@ -1,9 +1,39 @@
 // Version 0.0.1-alpha
 
 var ranges = [
-    '\ud83d[\ude00-\udeff]',
+    '\ud83d[\ude00-\udeff]', // unicode 6.0
     '\ud83d[\udc00-\udcff]',
-    '\ud83c[\udf00-\udff]'
+    '\ud83c[\udf00-\udfff]',
+    '\u23f0',
+    '\u270b',
+    '\u270a',
+    '\ud83d[\udd00-\uddff]',
+    '\u23f3',
+    '\u23f1\ufe0f',
+    '\u23f2\ufe0f',
+    '\u2728',
+    '\ud83c\udccf',
+    '\u26ce',
+    '\u23e9',
+    '\u23ed[\ufe00-\ufeff]',
+    '\u23ef\ufe0f',
+    '\u23ea',
+    '\u23ee\ufe0f',
+    '\u23eb',
+    '\u23ec',
+    '\u2705',
+    '\u274c',
+    '\u274e',
+    '\u2795',
+    '\u2796',
+    '\u2797',
+    '\u27b0',
+    '\u27bf',
+    '\u2753',
+    '\u2754',
+    '\u2755',
+    '\ud83c[\udd00-\uddff]',
+    '\ud83c[\ude00-\udeff]' // end unicode 6.0
 ];
 
 function removeEmojis(value){
